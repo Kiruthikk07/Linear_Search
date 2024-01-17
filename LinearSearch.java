9101aab03bbbd6fac4b1169308576d2e285f3d68
Kiruthik Kumar <122508631+Kiruthikk07@users.noreply.github.com>
2024-01-17 11:01:15 +0530

    LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LinearSeach{
+public class LinearSearch{
     public static int linearSearch(int[]arr, int target){
         for(int i = 0; i<arr.length; i++){
             if(arr[i] == target){
