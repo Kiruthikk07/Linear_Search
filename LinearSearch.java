595c08262d65b3aa7f48439bfa29dc77e6f7c810
Kiruthik Kumar <122508631+Kiruthikk07@users.noreply.github.com>
2024-01-17 10:47:38 +0530

    LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class LinearSeach{
+    public static int linearSearch(int[]arr, int target){
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == target){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int[]arr = {10,30,20,50,40};
+		System.out.println("Enter the element to search");
+		int element = sc.nextInt();
+		int pos = linearSearch(arr, element);
+		if(pos != -1){
+		    System.out.println("The element is found at position " + pos);
+		}
+		else{
+		    System.out.println("The element is not found");
+		}
+	}
+}
